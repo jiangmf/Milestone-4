12dc2f24542320c26963e87fba94991ffa1e7b19
unknown <you@example.com>
2018-11-29 13:26:09 -0500

    new songs

--- main.js
+++ main.js
@@ -1,34 +1,364 @@
 var songs = {
-  1: {
-    artist: "Kanye West",
-    title: "All of the Lights",
-    album: "My Beautiful Dark Twisted Fantasy",
-    duration: 300
-  },
-  2: {
-    artist: "BROCKHAMPTON",
-    title: "District",
-    album: "iridescence",
-    duration: 211
-  },
-  3: {
-    artist: "Kendrick Lamar",
-    title: "Humble",
-    album: "DAMN",
-    duration: 177
-  },
-  4: {
-    artist: "Travis Scott",
-    title: "Skyfall",
-    album: "Days Before Rodeo",
-    duration: 318
-  },
-  5: {
-    artist: "Denzel Curry",
-    title: "This Life",
-    album: "Imperial",
-    duration: 207
-  }
+	1: {
+	    artist: "Daniel Caesar",
+	    title: "Who Hurt You?",
+	    album: "Who Hurt You? - Single",
+	    duration: 231
+	  },
+	2: {
+	    artist: "Summer Walker",
+	    title: "Session 32",
+	    album: "Session 32 - Single",
+	    duration: 98
+	  },
+	3: {
+	    artist: "Khalid",
+	    title: "Saturday Nights",
+	    album: "Suncity",
+	    duration: 209
+	  },
+	4: {
+	    artist: "Ella Mai",
+	    title: "Everything (feat. John Legend)",
+	    album: "Ella Mai",
+	    duration: 181
+	  },
+	5: {
+	    artist: "Ari Lennox",
+	    title: "Grampa",
+	    album: "Grampa - Single",
+	    duration: 232
+	  },
+	6: {
+	    artist: "Teyana Taylor",
+	    title: "Issues / Hold On",
+	    album: "K.T.S.E.",
+	    duration: 185
+	  },
+	7: {
+	    artist: "Andra Day",
+	    title: "Amen",
+	    album: "Amen - Single",
+	    duration: 183
+	  },
+	8: {
+	    artist: "Jessie Reyez",
+	    title: "Apple Juice",
+	    album: "Being Human In Public",
+	    duration: 180
+	  },
+	9: {
+	    artist: "H.E.R.",
+	    title: "Could've Been (feat. Bryson Tiller)",
+	    album: "I Used to Know Her: The Prelude - EP",
+	    duration: 248
+	  },
+	10: {
+	    artist: "Alina Baraz",
+	    title: "Fallin",
+	    album: "The Color of You",
+	    duration: 246
+	  },
+	11: {
+	    artist: "Free Nationals",
+	    title: "Beauty & Essex (feat. Daniel Caesar & Unknown Mortal Orchestra)",
+	    album: "Beauty & Essex (feat. Daniel Caesar & Unknown Mortal Orchestra) - Single",
+	    duration: 277
+	  },
+	12: {
+	    artist: "Sabrina Claudio",
+	    title: "Messages From Her",
+	    album: "Messages From Her - Single",
+	    duration: 290
+	  },
+	13: {
+	    artist: "LOONY",
+	    title: "A Small Flame",
+	    album: "A Small Flame - Single",
+	    duration: 229
+	  },
+	14: {
+	    artist: "Queen Naija",
+	    title: "Butterflies",
+	    album: "Queen Naija - EP",
+	    duration: 212
+	  },
+	15: {
+	    artist: "Jorja Smith",
+	    title: "February 3rd",
+	    album: "Lost & Found",
+	    duration: 242
+	  },
+	16: {
+	    artist: "Ryland James",
+	    title: "Good to You",
+	    album: "Good to You - Single",
+	    duration: 189
+	  },
+	17: {
+	    artist: "Corinne Bailey Rae",
+	    title: "Like a Star",
+	    album: "Corinne Bailey Rae",
+	    duration: 243
+	  },
+	18: {
+	    artist: "The Weeknd",
+	    title: "Call Out My Name",
+	    album: "My Dear Melancholy,",
+	    duration: 228
+	  },
+	19: {
+	    artist: "Sabrina Claudio",
+	    title: "Frozen",
+	    album: "About Time",
+	    duration: 244
+	  },
+	20: {
+	    artist: "Eloise",
+	    title: "You, Dear",
+	    album: "You, Dear - Single",
+	    duration: 187
+	  },
+	21: {
+	    artist: "Leon Bridges",
+	    title: "Beyond",
+	    album: "Good Thing",
+	    duration: 240
+	  },
+	22: {
+	    artist: "Omar Apollo",
+	    title: "Erase",
+	    album: "Stereo",
+	    duration: 236
+	  },
+	23: {
+	    artist: "Justin Timberlake",
+	    title: "Morning Light (feat. Alicia Keys)",
+	    album: "Man of the Woods",
+	    duration: 243
+	  },
+	24: {
+	    artist: "Charlotte Day Wilson",
+	    title: "Doubt",
+	    album: "Doubt - Single",
+	    duration: 257
+	  },
+	25: {
+	    artist: "Alicia Keys",
+	    title: "If I Ain't Got You",
+	    album: "The Diary of Alicia Keys",
+	    duration: 228
+	  },
+	26: {
+	    artist: "Grace",
+	    title: "Boyfriend Jeans",
+	    album: "Boyfriend Jeans - Single",
+	    duration: 202
+	  },
+	27: {
+	    artist: "Khalid",
+	    title: "Stay",
+	    album: "Uncle Drew (Original Motion Picture Soundtrack)",
+	    duration: 204
+	  },
+	28: {
+	    artist: "H.E.R.",
+	    title: "Focus",
+	    album: "H.E.R.",
+	    duration: 200
+	  },
+	29: {
+	    artist: "YEBBA",
+	    title: "Evergreen",
+	    album: "Evergreen - Single",
+	    duration: 192
+	  },
+	30: {
+	    artist: "lemin.",
+	    title: "My Body",
+	    album: "My Body - Single",
+	    duration: 207
+	  },
+	31: {
+	    artist: "Ella Mai",
+	    title: "Naked",
+	    album: "Naked - Single",
+	    duration: 197
+	  },
+	32: {
+	    artist: "KIRBY",
+	    title: "Vain",
+	    album: "Vain - Single",
+	    duration: 245
+	  },
+	33: {
+	    artist: "Kehlani",
+	    title: "Again",
+	    album: "Again - Single",
+	    duration: 220
+	  },
+	34: {
+	    artist: "Daniel Caesar",
+	    title: "Blessed",
+	    album: "Freudian",
+	    duration: 241
+	  },
+	35: {
+	    artist: "JOY.",
+	    title: "Change",
+	    album: "Six",
+	    duration: 222
+	  },
+	36: {
+	    artist: "Jorja Smith",
+	    title: "Beautiful Little Fools",
+	    album: "Beautiful Little Fools - Single",
+	    duration: 216
+	  },
+	37: {
+	    artist: "Brent Faiyaz",
+	    title: "Talk 2 U",
+	    album: "Sonder Son",
+	    duration: 159
+	  },
+	38: {
+	    artist: "Leon Bridges",
+	    title: "Bet Ain't Worth the Hand",
+	    album: "Good Thing",
+	    duration: 185
+	  },
+	39: {
+	    artist: "Sade",
+	    title: "By Your Side",
+	    album: "Lovers Rock",
+	    duration: 274
+	  },
+	40: {
+	    artist: "dvsn",
+	    title: "Mood",
+	    album: "Morning After",
+	    duration: 215
+	  },
+	41: {
+	    artist: "Kehlani",
+	    title: "Honey",
+	    album: "Honey - Single",
+	    duration: 199
+	  },
+	42: {
+	    artist: "Sampha",
+	    title: "(No One Knows Me) Like the Piano",
+	    album: "Process",
+	    duration: 218
+	  },
+	43: {
+	    artist: "ODIE",
+	    title: "Little Lies",
+	    album: "Analogue",
+	    duration: 220
+	  },
+	44: {
+	    artist: "Bruno Mars",
+	    title: "When I Was Your Man",
+	    album: "Unorthodox Jukebox",
+	    duration: 213
+	  },
+	45: {
+	    artist: "Charlotte Day Wilson",
+	    title: "Find You",
+	    album: "CDW - EP",
+	    duration: 303
+	  },
+	46: {
+	    artist: "Daniel Caesar",
+	    title: "Get You (feat. Kali Uchis)",
+	    album: "Freudian",
+	    duration: 278
+	  },
+	47: {
+	    artist: "Lauryn Hill",
+	    title: "Nothing Even Matters (feat. D'Angelo)",
+	    album: "The Miseducation of Lauryn Hill",
+	    duration: 350
+	  },
+	48: {
+	    artist: "Lou Val",
+	    title: "The Earth Stood Still",
+	    album: "Lonely In Paradise",
+	    duration: 194
+	  },
+	49: {
+	    artist: "SiR",
+	    title: "Summer in November",
+	    album: "November",
+	    duration: 229
+	  },
+	50: {
+	    artist: "H.E.R.",
+	    title: "My Song",
+	    album: "My Song - Single",
+	    duration: 350
+	  },
+	51: {
+	    artist: "Jessie Reyez",
+	    title: "Figures, a Reprise (feat. Daniel Caesar)",
+	    album: "Figures, a Reprise (feat. Daniel Caesar) - Single",
+	    duration: 192
+	  },
+	52: {
+	    artist: "Ne-Yo",
+	    title: "GOOD MAN",
+	    album: "GOOD MAN (Deluxe)",
+	    duration: 229
+	  },
+	53: {
+	    artist: "Sean Leon",
+	    title: "90 BPM",
+	    album: "90 BPM - Single",
+	    duration: 149
+	  },
+	54: {
+	    artist: "Andra Day",
+	    title: "Rise Up",
+	    album: "Cheers to the Fall",
+	    duration: 253
+	  },
+	55: {
+	    artist: "Rihanna",
+	    title: "Stay (feat. Mikky Ekko)",
+	    album: "Unapologetic (Deluxe Version)",
+	    duration: 240
+	  },
+	56: {
+	    artist: "Childish Gambino",
+	    title: "Redbone",
+	    album: "Awaken, My Love!",
+	    duration: 326
+	  },
+	57: {
+	    artist: "PARTYNEXTDOOR",
+	    title: "The Right Way",
+	    album: "Seven Days",
+	    duration: 281
+	  },
+	58: {
+	    artist: "D%u2019Angelo and The Vanguard",
+	    title: "Really Love",
+	    album: "Black Messiah",
+	    duration: 344
+	  },
+	59: {
+	    artist: "Lily Moore",
+	    title: "Not That Special",
+	    album: "Not That Special - Single",
+	    duration: 219
+	  },
+	60: {
+	    artist: "The Weeknd",
+	    title: "True Colors",
+	    album: "Starboy",
+	    duration: 206
+	  },
 };
 
 var playlists = {
@@ -206,7 +536,7 @@ function showLibrary() {
   html += "</tbody>";
   table.html(html);
   $("#libraryWindow .songTable").html(table);
-  $('#libraryWindow .songTable.songList').dataTable({
+  $('#libraryWindow .songTable .songList').dataTable({
 		autoWidth: false,
     paging: false,
     searching: false,
